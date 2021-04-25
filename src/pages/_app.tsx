6a6bebba2b5b88cd096e5b3439f454ac064199d6
Eduardo Assunçao <cunhaeduardo1231@gmail.com>
2021-04-24 21:58:26 -0300

    refactor: decouple the codes related to the context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,34 +1,12 @@
-import { useState } from 'react';
 import { Header } from '../components/Header';
 import { Player } from '../components/Player';
-import { PlayerContext } from '../contexts/PlayerContext';
+import { PlayerContextProvider } from '../contexts/PlayerContext';
 import styles from '../styles/app.module.scss';
 import '../styles/global.scss';
 
 export default function MyApp({ Component, pageProps }) {
-  const [episodeList, setEpisodeList] = useState([]);
-  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  function play(episode) {
-    setEpisodeList([episode]);
-    setCurrentEpisodeIndex(0);
-    setIsPlaying(true);
-  }
-
-  function togglePlay() {
-    setIsPlaying(!isPlaying);
-  }
-
-  function setPlayingState(state: boolean) {
-    setIsPlaying(state);
-  }
-
   return (
-    <PlayerContext.Provider value={{
-      episodeList, currentEpisodeIndex, isPlaying, togglePlay, play, setPlayingState,
-    }}
-    >
+    <PlayerContextProvider>
       <div className={styles.wrapper}>
         <main>
           <Header />
@@ -36,6 +14,6 @@ export default function MyApp({ Component, pageProps }) {
         </main>
         <Player />
       </div>
-    </PlayerContext.Provider>
+    </PlayerContextProvider>
   );
 }
